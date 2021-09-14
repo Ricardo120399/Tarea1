796c3dd89b76fd97c570e9bfefe94100267a2313
Ricardo De La Cruz <68874460+Ricardo120399@users.noreply.github.com>
2021-09-13 23:30:45 -0400

    Add files via upload

---
+++ Electrodomesticos/Electrodomesticos/Aire.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electrodomesticos
+{
+    public class Aire
+    {
+        //atributos
+              //1
+        private string _marca;
+
+        public string GetMarca()
+        {
+            return _marca;
+        }
+        public void SetMarca(string valor)
+        {
+            _marca = valor;
+        }
+                 //2
+        private string _modelo;
+
+        public string GetModelo()
+        {
+            return _modelo;
+        }
+        public void SetModelo(string valor)
+        {
+            _modelo = valor;
+        }
+                //3
+        private int _precio;
+
+        public int GetPrecio()
+        {
+            return _precio;
+        }
+        public void SetPrecio(int valor)
+        {
+            _precio = valor;
+        }
+        //Metodos
+        public void Tamaño()
+        {
+            Console.WriteLine("El aire es bastante pequeña");
+
+        }
+        public void Procedencia()
+        {
+            Console.WriteLine("El aire fue hecho en China");
+        }
+        public void Color()
+        {
+            Console.WriteLine("El color es gris opaco...");
+        }
+    }
+}
---
+++ Electrodomesticos/Electrodomesticos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Electrodomesticos
+{
+    class Program
+    {
+      static void Main(string[] args)
+        {
+            Tostadora tostadora = new Tostadora();
+            tostadora.Ricardo();
+            Console.WriteLine("----------------------------------------");
+            tostadora.Tamaño();
+            tostadora.Procedencia();
+            tostadora.SetMarca("Toshiba");
+            Console.WriteLine(tostadora.GetMarca());
+            Console.WriteLine("----------------------------------------");
+            Radio radio = new Radio();
+            radio.Color();
+            radio.Procedencia();
+            radio.Tamaño();
+            Console.WriteLine("----------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
